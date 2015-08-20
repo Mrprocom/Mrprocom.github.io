b1cc6029ff84aa1c2fe0851a2afaaa55e81f6e8d
Mrprocom <mrprocom@hotmail.com>
2015-08-20 22:47:59 +0400

    Start a new project (IRC)

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
-    // Content
+    // This block switches the IRC client from KiwiIRC to QwebIRC
+    $(document).on("click", ".switch-qwebirc", function(){
+      $("#irc-client").attr("src", "https://webchat.freenode.net?uio=MTE9MjQ255");
+      $("#client-website").attr("href", "https://webchat.freenode.net");
+      $(this).html("Switch to KiwiIRC");
+      $(this).addClass("switch-kiwiirc");
+      $(this).removeClass("switch-qwebirc");
+    });
+
+    // This block switches the IRC client from QwebIRC to KiwiIRC
+    $(document).on("click", ".switch-kiwiirc", function(){
+      $("#irc-client").attr("src", "https://kiwiirc.com/client/irc.freenode.net/?&amp;theme=cli");
+      $("#client-website").attr("href", "https://kiwiirc.com");
+      $(this).html("Switch to QwebIRC");
+      $(this).addClass("switch-qwebirc");
+      $(this).removeClass("switch-kiwiirc");
+    });
 
 });
