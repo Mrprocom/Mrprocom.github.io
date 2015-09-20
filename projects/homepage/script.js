fa84a8dc46b8b8e0c48e57921f58a57907550e32
Mrprocom <mrprocom@hotmail.com>
2015-09-20 13:45:10 +0400

    Link main-page.html and custom.html to homepage.html

--- projects/homepage/script.js
+++ projects/homepage/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+  $("#switch-main").on("click", function(){
+    $("#iframe-screen").attr("src", "main-page.html");
+  });
 
+  $("#switch-custom").on("click", function(){
+    $("#iframe-screen").attr("src", "custom.html")
+  });
 
 });
